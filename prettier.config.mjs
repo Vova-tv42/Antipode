4f0ea3ff740a804d887b830682bbfeead786c27f
Volodymyr <tv42.konopl.vova@gmail.com>
2026-02-01 20:06:53 +0200

    feat: Configure shadcn/ui and development tooling.

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 100,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default config;
